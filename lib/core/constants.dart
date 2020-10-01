9410206234125831a479aa4080bd9b3ef1ac20e1
sarang m <sarangsrg10@gmail.com>
2020-10-01 14:55:24 +0530

    modified documentation

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -3,13 +3,14 @@ import 'package:todoapp/core/screen_size.dart';
 
 const KBackgroundColor = Colors.lightBlueAccent;
 
+const KDarkBackgroundColor = Colors.black26;
 
 final KHeadingTextStyle = TextStyle(
-  color: Colors.white,
-  fontSize:SizeConfig.safeBlockHorizontal*12,
-  fontWeight: FontWeight.w600
-);
+    color: Colors.white,
+    fontSize: SizeConfig.safeBlockHorizontal * 12,
+    fontWeight: FontWeight.w600);
 
-final KTaskTextStyle =TextStyle(fontSize: SizeConfig.safeBlockHorizontal*5,
+final KTaskTextStyle = TextStyle(
+    fontSize: SizeConfig.safeBlockHorizontal * 5,
     color: Colors.black,
     fontWeight: FontWeight.w500);
